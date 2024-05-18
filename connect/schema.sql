5e63cd52522ff9b92db2ac2f5d325f6cd186da67
Maro Jabor <andrewjabor@gmail.com>
2024-05-18 15:10:40 +0000

    connect task

---
+++ connect/schema.sql
@@ -0,0 +1,54 @@
+CREATE TABLE "users" (
+    "id" INTEGER,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "username" TEXT,
+    "password" NUMERIC NOT NULL,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "schools_and_universities" (
+    "id" INTEGER,
+    "name" TEXT UNIQUE,
+    "type" TEXT,
+    "location" TEXT
+    "year_founded" NUMERIC,
+    PRIMARY KEY("id")
+);
+
+CREATE TABLE "companies" (
+    "id" INTEGER,
+    "name" TEXT UNIQUE,
+    "industry" TEXT,
+    "location" TEXT,
+    PRIMARY KEY("id") 
+);
+
+CREATE TABLE "connections_with_people" (
+    "id" INTEGER,
+    "connector_1" INTEGER,
+    "connector_2" INTEGER,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY("connector_1") REFERENCES "users"("id") ,
+    FOREIGN KEY("connector_2") REFERENCES "users"("id")
+);
+
+CREATE TABLE "connections_with_school" (
+    "id" INTEGER,
+    "school_id" INTEGER,
+    "start-date" NUMERIC,
+    "end_date" NUMERIC,
+    "degree" TEXT,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY("school_id") REFERENCES "school_and_universities"("id")
+);
+
+CREATE TABLE "connections_with_company" (
+    "id" INTEGER,
+    "company_id" INTEGER,
+    "start-date" NUMERIC,
+    "end_date" NUMERIC,
+    "title_held" TEXT,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY("company_id") REFERENCES "companies"("id")
+);
